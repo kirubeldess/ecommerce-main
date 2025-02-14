eb795eeb1445dcf114335ef94e6f6f3bd453c8e4
Kirubel <kirubeldesalegn17@gmail.com>
2025-02-14 11:47:51 +0300

    product detail page

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,5 @@
 export const APP_NAME=process.env.NEXT_PUBLIC_APP_NAME || 'ECOM_MEK'
 export const APP_DESCRIPTION=process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'at your door steps.'
 export const APP_SLOGAN=process.env.NEXT_PUBLIC_APP_SLOGAN || 'your ultimate e-commerce destination!'
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
