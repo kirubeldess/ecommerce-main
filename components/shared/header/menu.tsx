d5bea3ec67f335a3706f7b4d81d80bd7215e7042
Kirubel <kirubeldesalegn17@gmail.com>
2025-02-23 14:26:55 +0300

    admin dashboard

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -3,19 +3,12 @@
 import CartButton from "./cart-button";
 import UserButton from "./user-button";
 
-export default function Menu(){
+export default function Menu({forAdmin = false} : {forAdmin?:boolean}){
     return (
         <div className="flex justify-end">
             <nav className="flex gap-3 w-full items-center">
-                {/* <Link href='/signin' className="header-button place-items-center ">
-                    <span className="font-semibold ">Hello, Sign in</span>
-                </Link> */}
                 <UserButton />
-                {/* <Link href='/cart' className="header-buton ">
-                    <ShoppingCartIcon className="h-6 w-6 text-primary"/>
-                    <span className="font-semibold ">Cart</span>
-                </Link> */}
-                <CartButton/>
+                {forAdmin? null : <CartButton/>}
             </nav>
         </div>
     )
--- types/index.ts
+++ types/index.ts
@@ -35,6 +35,14 @@ export type Data={
 }
 
 export type IOrderInput = z.infer<typeof OrderInputSchema>
+export type IOrderList = IOrderInput & {
+    _id: string
+    user: {
+      name: string
+      email: string
+    }
+    createdAt: Date
+}
 export type OrderItem = z.infer<typeof OrderItemSchema>
 export type Cart = z.infer<typeof CartSchema>
 export type ShippingAddress = z.infer<typeof ShippingAddressSchema>
@@ -44,3 +52,4 @@ export type IUserSignIn = z.infer<typeof UserSignInSchema>
 
 export type IUserSignUp = z.infer<typeof UserSignUpSchema>
 export type IUserName = z.infer<typeof UserNameSchema>
+
