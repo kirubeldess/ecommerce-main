b0799101fc0448cc743f273379b972c62e559973
Kirubel <kirubeldesalegn17@gmail.com>
2025-02-23 11:59:00 +0300

    sidebar

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -2,10 +2,14 @@ import { APP_NAME } from "@/lib/constants";
 import Link from "next/link";
 import Search from "./search";
 import Menu from "./menu";
-import { Button } from "@/components/ui/button";
-import { MenuIcon } from "lucide-react";
+// import { Button } from "@/components/ui/button";
+// import { MenuIcon } from "lucide-react";
 import data from "@/lib/data";
+import Sidebar from "./sidebar";
+import { getAllCategories } from "@/lib/actions/product.actions";
 
+
+const categories = await getAllCategories()
 export default function Header(){
     return(
         <header className=" text-black">
@@ -27,10 +31,12 @@ export default function Header(){
             </div>
             <div className="border-t border-gray-300 mx-3"></div>
             <div className="flex items-center px-3 mb-[1px] ">
-                <Button variant='ghost' className=" header-button flex items-center gap-1 text-base [&_svg]:size-6 hover:bg-primary hover:text-white  hover:rounded-md">
+                {/* <Button variant='ghost' className=" header-button flex items-center gap-1 text-base [&_svg]:size-6 hover:bg-primary hover:text-white  hover:rounded-md">
                     <MenuIcon/>
                     All
-                </Button>
+                </Button> */}
+                
+                <Sidebar categories={categories} />
                 <div className="flex items-center flex-wrap gap-3 overflow-hidden max-h-[42px]">
                     {data.headerMenus.map((menu)=>(
                         <Link href={menu.href} key={menu.href} className='header-button !p-2 hover:bg-primary hover:text-white transition-colors duration-500 ease-in-out  hover:rounded-md'>
